2f89fc78dce0dc9cc27bf529fb23395a74516f54
austinmpask <austinmpask@gmail.com>
2024-01-29 13:56:23 -0800

    done

--- assets/js/manipulate.js
+++ assets/js/manipulate.js
@@ -1,18 +1,19 @@
-function changeTitle() {
-    // Change the title of the page to "(Your name)'s Portfolio"
-
-    // Your code here
+export function changeTitle() {
+  // Change the title of the page to "(Your name)'s Portfolio"
+  // Your code here
+  window.document.title = "Joe schmo";
 }
 
-function changeHeader() {
-    // Change the name in the h1 of the page to your name
-
-    // Your code here
+export function changeHeader() {
+  // Change the name in the h1 of the page to your name
+  // Your code here
+  window.document.body.children[0].children[0].innerHTML = "joe joe joe";
 }
 
-function changeAboutMe() {
-    /* Update the first paragraph in the About Me section with a small
+export function changeAboutMe() {
+  /* Update the first paragraph in the About Me section with a small
      passage about yourself */
-
-    // Your code here
+  // Your code here
+  window.document.body.children[1].children[1].innerHTML =
+    "sdfsdfsdjhfsdkjfghsdjkfhksjdhfsjkdh";
 }
